65e29d6ad77b65fcc86d5968b77c09a4ebf99cba
sunkay <sunkay11@gmail.com>
2017-08-27 20:08:18 -0400

    Saving auth user into mongodb

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,9 @@
 const passport = require("passport");
+const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +13,14 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
+      User.findOne({ profileId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          console.log("user already exists");
+        } else {
+          console.log("saving profile with id: ", profile.id);
+          new User({ profileId: profile.id }).save();
+        }
+      });
     }
   )
 );
