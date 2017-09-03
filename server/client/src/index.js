464a7190c5f5077488ffcfed1c721f73c0c13549
sunkay <sunkay11@gmail.com>
2017-09-03 07:37:15 -0400

    redux

---
+++ server/client/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
